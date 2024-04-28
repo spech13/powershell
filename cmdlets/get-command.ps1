a0af671ea4dbe890d79e61bffcad6ad4550842e0
spech13 <es-popov2@yandex.ru>
2024-04-28 10:36:20 +0300

    Add get-command and get-help cmdlets description

---
+++ cmdlets/get-command.ps1
@@ -0,0 +1,4 @@
+# get-command - gets all commands that are installed on the computer, including cmdlets, aliases, functions, filters, scripts, and applications
+# get-command <cmdlet> -showcommandinfo - information getting about "cmdlet"
+
+get-command new-item -showcommandinfo
---
+++ cmdlets/get-help.ps1
@@ -0,0 +1,3 @@
+# get-help <any> - get information about cmdlets and concepts
+
+get-help new-item
